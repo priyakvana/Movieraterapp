2a29c218670f593a83e3efe834b4ed17484efcba
niranjan <niranjan.kvana@gmail.com>
2019-07-12 17:01:12 +0530

    changes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,7 +9,7 @@ import { $ } from 'protractor';
 })
 export class ApiService {
  
-  baseUrl ='http://127.0.0.1:8000/';
+  baseUrl ='https://angulardjangosample.herokuapp.com/';
   baseMovieUrl =`${this.baseUrl}api/movies/`;
   headers = new HttpHeaders({
     'Content-type':'application/json',
