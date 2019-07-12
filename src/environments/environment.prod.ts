748b8500ceddcf05964bb42b97e34533a17eda51
niranjan <niranjan.kvana@gmail.com>
2019-07-12 16:30:09 +0530

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'https://angulardjangosample.herokuapp.com/admin/api/movie/'
+  apiUrl:'https://angulardjangosample.herokuapp.com/api/'
 };
